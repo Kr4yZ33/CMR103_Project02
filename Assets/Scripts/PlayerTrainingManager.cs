12cc7e27306be6b09d6c8eee49c04aeb8064f77c
jpernesz <1020894@student.sae.edu.au>
2020-12-03 04:03:27 +1100

    Train room built, adding UI guides and player training

---
+++ Assets/Scripts/PlayerTrainingManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrainingManager : MonoBehaviour
+{
+    public GameObject leftTrackGap;
+    public GameObject rightTrackGap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableTrackGapGuides()
+    {
+        leftTrackGap.SetActive(true);
+        rightTrackGap.SetActive(true);
+    }
+
+    public void DisableTrackGapGuides()
+    {
+        leftTrackGap.SetActive(false);
+        rightTrackGap.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject uIControls;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("RightHand"))
+        {
+            uIControls.SetActive(false);
+        }
+    }
+
+    public void TurnOnUiControls()
+    {
+        uIControls.SetActive(true);
+    }
+
+    public void TurnOffUiControls()
+    {
+        uIControls.SetActive(false);
+    }
+}
