8f51041d8efdbc681c5295b9430c2de6d0d8a9b0
jpernesz <1020894@student.sae.edu.au>
2020-12-03 02:00:22 +1100

    Setup track snap grid

---
+++ Assets/Scripts/SnapObjectRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapObjectRotation : MonoBehaviour
+{
+    public int increment;
+    private void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, (Mathf.Round(transform.eulerAngles.y / increment) * increment), transform.eulerAngles.z);
+    }
+}
