3da3639d57e96ffadabe7e9b2e0cd54fbd954564
jpernesz <1020894@student.sae.edu.au>
2020-12-03 12:31:30 +1100

    Added audio on object grab, also added hover change script to change grab object colour on hover enter and exit

---
+++ Assets/Scripts/HoverChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverChange : MonoBehaviour
+{
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    public void MakeYellow()
+    {
+        r.material.color = Color.yellow;
+    }
+
+    public void MakeGrey()
+    {
+        r.material.color = Color.grey;
+    }
+}
