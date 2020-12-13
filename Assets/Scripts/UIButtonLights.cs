18b4533d42df87e95a3b86fa700a370f9f07b766
jpernesz <1020894@student.sae.edu.au>
2020-12-13 19:28:06 +1100

    Added gallery wall with pictures. Also added UI lights
    UI lights show when an option is enabled or disbaled with green or red colours

---
+++ Assets/Scripts/UIButtonLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonLights : MonoBehaviour
+{
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    public void MakeRed()
+    {
+        r.material.color = Color.red;
+    }
+
+    public void MakeGreen()
+    {
+        r.material.color = Color.green;
+    }
+}
